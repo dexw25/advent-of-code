cd8ddaa0de9782b0edc14cd09d6350e83a5da040
Derek Witt <derekw023@gmail.com>
2019-12-09 00:41:03 -0500

    Add constraint and solve day 4 part 2

--- 2019/4/main.rs
+++ 2019/4/main.rs
@@ -18,7 +18,7 @@ fn increasing(seq: &Vec<usize>) -> bool {
 	true
 }
 
-fn has_pair(seq: &Vec<usize>) -> bool {
+fn has_repeats(seq: &Vec<usize>) -> bool {
 	let mut last:usize = 10; // No digit in the input will be 10 or more
 	for i in seq.iter() {
 		if last == *i {return true;}
@@ -27,8 +27,37 @@ fn has_pair(seq: &Vec<usize>) -> bool {
 	false
 }
 
+// Part 2 constriction, return false if any sequence has more than 2 in a row
+fn has_pair(seq: &Vec<usize>) -> bool {
+	let mut last:usize = 10;
+	let mut i = 0;
+	while i < seq.len() {
+		let mut run_len = 0;
+
+		// Filter all repeats greedily
+		while i < seq.len() && last == seq[i]{
+			run_len += 1;
+			last = seq[i];
+			i += 1;
+		}
+		if run_len == 1 {return true;}
+
+		if i < seq.len() {last = seq[i];}
+		i += 1;
+	}
+	false
+}
+
 fn main() -> std::io::Result<()> {
+
+	// Some test cases
+	assert_eq!(has_repeats(&vec![1, 1, 1, 1, 1, 1]), true);
+	assert_eq!(has_pair(&vec![1, 1, 1, 1, 1, 1]), false);
+	assert_eq!(has_pair(&vec![1, 1, 2, 2, 3, 3]), true);
+	assert_eq!(has_pair(&vec![1, 1, 1, 2, 3, 3]), true);
+
 	let mut total:u32 = 0;
+	let mut total_2:u32 = 0;
 	let val_min = 367479;
 	let val_max = 893698; // this is the puzzle input
 	// 6 digit number enforced by loops
@@ -43,9 +72,12 @@ fn main() -> std::io::Result<()> {
 							let password_val = a * 100000 + b * 10000 + c * 1000 + d * 100 + e * 10 + f;
 							// If the number isnt in the given range then do not do any further checks
 							if password_val > val_min && password_val < val_max {
-								if increasing(&password) && has_pair(&password) {
+								if increasing(&password) && has_repeats(&password) {
 									total += 1;
 								} 
+								if increasing(&password) && has_repeats(&password) && has_pair(&password) {
+									total_2 += 1;
+								}
 							}
 						}
 					}
@@ -54,7 +86,8 @@ fn main() -> std::io::Result<()> {
 		}
 	}
 
-	println!("{}", total);
+	println!("Part 1: {}", total);
+	println!("Part 2: {}", total_2);
 
 	Ok(())
 }
