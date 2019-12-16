5c75eb2076b630b5d44db717de89aa56320fc765
Derek Witt <derekw023@gmail.com>
2019-12-15 20:16:40 -0500

    Partial implementation of the solution

--- 2019/10/main.rs
+++ 2019/10/main.rs
@@ -3,46 +3,59 @@ use std::io::Read;
 use std::collections::HashSet;
 use std::fmt;
 use std::convert::TryInto;
+use std::iter::FromIterator;
 
 // Asteroid field representation, a hash set of all of the asteroid coordinates
 struct Field {
 	rocks: HashSet<(u32, u32)>, // (x, y)
+	x_max: u32,
+	y_max: u32
 }
 
 impl Field {
 	fn new (input: &String) -> Field {
+		let mut x = 0;
+		let mut y = 0;
 		let mut map: HashSet<(u32, u32)> = HashSet::new();
 
 		// Parse the input grid row by row, a # is a rock, a . is nothing 
 		for (row, l) in input.lines().enumerate() {
+			if y < row {y = row;}
 			for (col, c) in l.chars().enumerate() {
 				match c {
 					'#' => map.insert((col.try_into().unwrap(), row.try_into().unwrap())),
 					_ => true
 				};
+				// This doesn't need to run except for the first line, but will expand input to allow for non uniform line lengths
+				if x < col {x = col;}
 			}
 		}
-		Field {rocks: map}
+		Field {rocks: map, x_max: x.try_into().unwrap(), y_max: y.try_into().unwrap()}
+	}
+
+	fn count_visible(&self, point: (u32, u32)) -> u32 {
+		// Make sure the set contains the passed point
+		assert_eq!(self.rocks.contains(&point), true);
+
+		// Copy the set minus point argument
+		let mut local_set: HashSet<&(u32, u32)> = HashSet::from_iter(self.rocks.iter());
+		local_set.remove(&point);
+
+		// for pt in local_set: remove all points that are occluded by this point
+		0
 	}
 }
 
 impl fmt::Display for Field {
 	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-		// Find bounds for the grid
-		let mut x_max = 0;
-		let mut y_max = 0;
-		for a in self.rocks.iter() {
-			if x_max < a.0 {x_max = a.0;}
-			if y_max < a.1 {y_max = a.1;}
-		}
-		for y in 0..=y_max {
-			for x in 0..x_max {
+		for y in 0..=self.y_max {
+			for x in 0..self.x_max {
 				write!(f, "{}", match self.rocks.contains(&(x, y)) {
 					true => "#",
-					false => " ",
+					false => " ",// whitespace for not-asteroids
 				})?;
 			}
-			writeln!(f, "")?;
+			writeln!(f, "")?; // newlines in between each row
 		}
 		Ok(())
     }
