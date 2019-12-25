3a954a48a140cdd84dfb9f90713784ae25277a5e
Derek Witt <derekw023@gmail.com>
2019-12-25 01:37:45 -0500

    Implement LCM based solution, down from <never waited for it to finish> to 2 seconds

--- 2019/12/main.rs
+++ 2019/12/main.rs
@@ -1,8 +1,9 @@
 use std::fs;
 use std::fmt;
+use std::collections::HashSet;
 
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+#[derive(Copy, Clone, PartialEq, Eq)]
 struct Coord {
 	x: i64,
 	y: i64,
@@ -15,7 +16,7 @@ impl Coord {
 	}
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Clone, PartialEq, Eq)]
 struct Moon {
 	pos: Coord,
 	vel: Coord,
@@ -116,29 +117,95 @@ impl Sim {
 	//  - Any loop point (that we care about) has zero velocity
 	//  - 
 	fn loop_len(&mut self) -> u64 {
-		// Compare total energy to begin, then check intensively to verify false positives
-		let mut total:u64 = 0;
-		let initial_state = self.bodies.clone();
-		let initial_energy =self.total_energy();
-		loop {
-			self.apply_gravity();
-			self.apply_velocity();
-			total += 1;
+		// Step each single axis to find the loop period of each and then do LCM search
+		// In theory this closure might be parallelized but this runs fast enough it doesn't need to be
+		let single_axis_search = |istate: &Vec<Moon>, axis| -> u64 {
+			let mut state: Vec<(i64, i64)> = vec![];
 
-			// Check if we looped by checksum (it's not a great checksum but it reduces the number of intensive checks)
-			let mut eq = true;
-			if initial_energy == self.total_energy() {
-				// Intensive check to eliminate false positives
-				// Vector is never reordered so compare element by element
-				for i in 0..self.bodies.len() {
-					if self.bodies[i] != initial_state[i] {eq = false;}
+			for m in istate {
+				// Select X Y or Z coord based on argument, this is thie only part that cares about axis
+				state.push(match axis {
+					Axes::X => (m.pos.x, 0),
+					Axes::Y => (m.pos.y, 0),
+					Axes::Z => (m.pos.z, 0),
+				});
+			}
+			let initstate = state.clone();
+			let mut steps = 0;
+
+			loop {
+				// Apply gravity
+				for i in 0..state.len() {
+					let mut accel = 0;
+					let pos = state[i].0; // 0 is position, 1 is vel
+					for j in 0..state.len() {
+						if j != i {
+							// add one for every body above this one, sub for every body below, ignore bodies at equal position
+							accel += match pos - state[j].0 {
+								1..=std::i64::MAX => -1,
+								std::i64::MIN..=-1 => 1,
+								0 => 0
+							};
+						}
+					};
+					state[i].1 += accel; // Update V
 				}
-				if eq {
+
+				// Apply velocity
+				for i in 0..state.len() {
+					state[i].0 += state[i].1;
+				}
+
+				steps += 1;
+
+				// Check if cycle got found
+				let mut cycle = true;
+				for i in 0..state.len() {
+					if state[i].1 != 0 {
+						cycle = false;
+						break;
+					} else if state[i].0 != initstate[i].0 {
+						cycle = false;
+						break;
+					}
+				}
+				if cycle {
 					break;
 				}
 			}
+			steps
+		};
+
+		// Call the above closure on each axis
+		let per_x = single_axis_search(&self.bodies, Axes::X);
+		let per_y = single_axis_search(&self.bodies, Axes::Y);
+		let per_z = single_axis_search(&self.bodies, Axes::Z);
+
+		// Periods of each axis discovered, since motion on any individual axis is entirely periodic, we can find the LCM of these numbers 
+		//  And this is the period of the combination of all 3 subsystems
+		let max_per = per_x.max(per_y.max(per_z));
+
+		let p = primes(max_per);
+
+		// LCM algorithm, decompose the 3 inputs into prime factors
+		let x_dec = decompose(per_x, &p);
+		let y_dec = decompose(per_y, &p);
+		let z_dec = decompose(per_z, &p);
+
+		// LCM is simply the result of the max of each prime factor, recomposed
+		let mut res: Vec<u32> = Vec::with_capacity(p.len());
+		for _i in 0..p.len() {res.push(0);} // initialize
+		// for each prime slot store max of all 3 inputs
+		for i in 0..res.len() {
+			res[i] = x_dec[i].max(y_dec[i].max(z_dec[i]));
 		}
-		total
+
+		// Recompose
+		let mut ret = 1;
+		for (i, v) in res.iter().enumerate() {
+			ret *= p[i].pow(*v);
+		}
+		ret
 	}
 }
 
@@ -169,10 +236,49 @@ impl fmt::Display for Moon {
     }
 }
 
+// Random math
+fn primes(top: u64) -> Vec<u64>{
+	let mut multiples: HashSet<u64> = HashSet::new();
+	let mut ret: Vec<u64> = Vec::new();
+	for i in 2..top/2 {
+		// Make sure i is not a multiple of a prime we've seen already
+		if multiples.contains(&i) {continue;}
+		// else i is a new prime
+		else {
+			ret.push(i);
+			let mut acc = i;
+			// Cache all multiples of i in hash set
+			loop {
+				if acc > top/2 {break;}
+				acc += i;
+				multiples.insert(acc);
+			}
+		}
+	}
+	ret
+}
+
+// Decompose n to prime factors
+fn decompose(mut n: u64, primes: &Vec<u64>) -> Vec<u32> {
+	let mut ret: Vec<u32> = Vec::with_capacity(primes.len());
+	for _i in 0..primes.len() {ret.push(0);} // init with zeros
+	let mut i = primes.len()-1;
+	while n > 1 {
+		// if primes[i] divides n, add one to count and reduce n
+		if n % primes[i] == 0 {
+			n /= primes[i];
+			ret[i] += 1;
+		} else {
+			// Else if remainder, dec i
+			i -= 1;
+		}
+	}
+
+	ret
+}
+
 fn main() -> std::io::Result<()> {
 	let buf = &fs::read("./initstate.txt")?;
-
-	let moons: Vec<Moon> = std::str::from_utf8(buf).unwrap().trim().lines().map(|s| Moon::from(s)).collect();
 
 	// Initialize sim with state from file: For each line create a new moon object and make a vector of them all
 	let mut sim = Sim::new(std::str::from_utf8(buf).unwrap().trim().lines().map(|s| Moon::from(s)).collect());
@@ -185,9 +291,7 @@ fn main() -> std::io::Result<()> {
 	// Reset sim
 	let mut sim = Sim::new(std::str::from_utf8(buf).unwrap().trim().lines().map(|s| Moon::from(s)).collect());
 	
-
 	println!("{} steps until loop", sim.loop_len());
-
 
 	Ok(())
 }
