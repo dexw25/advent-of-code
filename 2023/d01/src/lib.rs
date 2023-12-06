505a4c9d98919b01b62c4161d28174265f259f18
Dex Witt <dex@witt.me>
2023-12-06 18:22:56 -0500

    d1p2

--- 2023/d01/src/lib.rs
+++ 2023/d01/src/lib.rs
@@ -1,3 +1,6 @@
+use core::str::Chars;
+use std::collections::HashMap;
+
 #[must_use]
 pub fn sum_calibrations(input: &str) -> u32 {
     input
@@ -14,12 +17,79 @@ pub fn sum_calibrations(input: &str) -> u32 {
         .sum()
 }
 
-#[must_use]
-pub fn sum_calibrations_v2(input: &str) -> u32 {}
+pub struct Tokenizer<'a> {
+    source: Chars<'a>,
+    token_map: HashMap<&'static str, u32>,
+}
+
+// This won't work for unicode. None of the below will
+pub fn tokenize(input: &str) -> impl Iterator<Item = u32> + '_ + DoubleEndedIterator {
+    Tokenizer {
+        source: input.chars(),
+        token_map: HashMap::from([
+            ("one", 1),
+            ("two", 2),
+            ("three", 3),
+            ("four", 4),
+            ("five", 5),
+            ("six", 6),
+            ("seven", 7),
+            ("eight", 8),
+            ("nine", 9),
+        ]),
+    }
+}
+
+impl Iterator for Tokenizer<'_> {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            // Borrow the full string for later
+            let s = self.source.as_str();
+            let c = self.source.next()?;
+
+            if c.is_ascii_digit() {
+                // this WILL be Some(...) because of the check above
+                return c.to_digit(10);
+            }
+            for tok in self.token_map.keys() {
+                if s.starts_with(tok) {
+                    // seek the number of chars in the token, minus 1 for zero-indexing, minus 1 for the character already seeked at the top of this
+                    self.source.nth(tok.len() - 2);
+                    return self.token_map.get(tok).copied();
+                }
+            }
+        }
+    }
+}
+
+impl DoubleEndedIterator for Tokenizer<'_> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        loop {
+            // Borrow the full string for later
+            let s = self.source.as_str();
+            let c = self.source.next_back()?;
+
+            if c.is_ascii_digit() {
+                // this WILL be Some(...) because of the check above
+                break c.to_digit(10);
+            }
+            for tok in self.token_map.keys() {
+                if s.ends_with(tok) {
+                    // seek the number of chars in the token, minus 1 for zero-indexing, minus 1 for the character already seeked at the top of this
+                    self.source.nth_back(tok.len() - 2);
+                    return self.token_map.get(tok).copied();
+                }
+            }
+        }
+    }
+}
 
 #[cfg(test)]
 mod test {
     use super::sum_calibrations;
+    use super::tokenize;
 
     #[test]
     fn sample1() {
@@ -28,7 +98,28 @@ mod test {
     }
 
     #[test]
+    fn forward_tokenize() {
+        assert_eq!(tokenize("two1nine").next(), Some(2));
+        assert_eq!(tokenize("xtwone3four").next(), Some(2));
+    }
+
+    #[test]
+    fn reverse_tokenize() {
+        assert_eq!(tokenize("two1nine").next_back(), Some(9));
+        assert_eq!(tokenize("xtwone3four").next_back(), Some(4));
+        assert_eq!(tokenize("7pqrstsixteen").next_back(), Some(6));
+    }
+
+    #[test]
     fn sample2() {
-        assert_eq!(sum_calibrations_v2(include_str!("test2.txt")), 281);
+        let input = include_str!("test2.txt").lines().map(tokenize);
+        let sum: u32 = input
+            .map(|mut l| {
+                let a = l.next().unwrap();
+                let b = l.next_back().unwrap_or(a);
+                a * 10 + b
+            })
+            .sum();
+        assert_eq!(sum, 281);
     }
 }
--- 2023/d01/src/main.rs
+++ 2023/d01/src/main.rs
@@ -1,5 +1,17 @@
 use d01::sum_calibrations;
 
 fn main() {
-    dbg!(sum_calibrations(include_str!("input.txt")));
+    let input = include_str!("input.txt");
+    dbg!(sum_calibrations(input));
+    let sum2: u32 = input
+        .lines()
+        .map(d01::tokenize)
+        .map(|mut l| {
+            let a = l.next().unwrap();
+            let b = l.next_back().unwrap_or(a);
+            a * 10 + b
+        })
+        .sum();
+
+    dbg!(sum2);
 }
